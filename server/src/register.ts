1ba1c079c4df1e58e6535cc9cede5b15ec85f440
Michal Sklenár <michal.sklenar@sklinet.com>
2024-11-01 10:26:32 +0100

    fix field type

--- server/src/register.ts
+++ server/src/register.ts
@@ -7,7 +7,7 @@ const register = ({ strapi }: { strapi: Core.Strapi }) => {
   strapi.customFields.register({
     name: 'tinymce',
     plugin: PLUGIN_ID,
-    type: 'string',
+    type: 'richtext',
   });
 };
 
